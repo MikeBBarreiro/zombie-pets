c0f7154a14929fd337982a9f05040fc71519f4fb
Michael Barreiro <Mikeb_barreiro@yahoo.com>
2014-09-09 13:37:57 -0500

    form 2

--- client/index.js
+++ client/index.js
@@ -6,6 +6,34 @@
     var occupations = ['Distruction', 'Fluffyness', 'Danger', 'Chaos', 'Fluff', 'More Fluff', 'Lazers', 'Transformers', 'Madness'];
 
     $scope.title = 'zombie-pets';
+
+    $scope.weapon = {};
+    $scope.weapons = [];
+    $scope.pet = {};
+    $scope.pet = {health:100};
+    $scope.pets = [];
+
+    $scope.addPet = function(){
+      var index = $scope.pet.weapon * 1;
+      $scope.pet.weapon = $scope.weapons[index];
+      $scope.pets.push($scope.pet);
+      $scope.pet = {health:100};
+    };
+
+    $scope.addWeapon = function(){
+      $scope.weapons.push($scope.weapon);
+      $scope.weapon = {};
+      $('#name').focus();
+    };
+
+    $scope.toggleWeapon = function(){
+      $scope.hideWeapon = !!!$scope.hideWeapon;
+    };
+
+    $scope.togglePet = function(){
+      $scope.hidePet = !!!$scope.hidePet;
+    };
+
     $scope.occupation = occupations[0];
 //$intervals is like a loop
     $interval(function(){
